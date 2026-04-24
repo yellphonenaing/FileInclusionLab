cc9381d8da107d71df222306033dafe0e39a7821
ypn007 <55645320+ypn007@users.noreply.github.com>
2026-04-24 13:03:07 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+#Modify PHP configuration file for easy exploits
+sed -i 's/^\s*allow_url_include\s*=.*/allow_url_include = On/' /etc/php/8.2/apache2/php.ini
+
 # Start Apache in the foreground
 apachectl -D FOREGROUND
